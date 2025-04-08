514384fa170c1a5e1061a953eab8d1099b7aabca
Jenna Barton <jenna.barton@education.gov.uk>
2025-04-08 12:02:51 +0100

    Remove setupsession file as files now exist on server. Update SecirityChecks test to use session login

--- tests/cypress/e2e/Admin/ABC123SetUpSessionRequiredFiles.spec.ts
+++
@@ -1,6 +0,0 @@
-describe('Ensure file location for cookies exists', () => {
-    it("will return an error message if the bulk file contains more than 250 rows of data", () => {
-        cy.storeCookies('school');
-        cy.storeCookies('LA');
-    });
-});
