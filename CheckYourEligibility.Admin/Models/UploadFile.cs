62329c8f622c2b3367bb0c7a563fe3c57a317524
Jenna Barton <jenna.barton@education.gov.uk>
2025-04-10 12:44:50 +0100

    Create UploadFile model

---
+++ CheckYourEligibility.Admin/Models/UploadFile.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using CheckYourEligibility.Admin.Attributes;
+
+namespace CheckYourEligibility.Admin.Models;
+
+public class UploadFile
+{
+    [NotMapped] public int FileIndex { get; set; }
+
+    public string? FileName { get; set; }
+
+    public string? FileType { get; set; }
+}
