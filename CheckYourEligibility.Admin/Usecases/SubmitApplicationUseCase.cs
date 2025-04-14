4646e711c1698c33e7002c08c853dfc96e10bb89
Anoop T (DfE) <anoop.tatti@gmail.com>
2025-04-14 16:55:34 +0100

    feat: added code related to blob storage

--- CheckYourEligibility.Admin/Usecases/SubmitApplicationUseCase.cs
+++ CheckYourEligibility.Admin/Usecases/SubmitApplicationUseCase.cs
@@ -1,5 +1,6 @@
 using CheckYourEligibility.Admin.Boundary.Requests;
 using CheckYourEligibility.Admin.Boundary.Responses;
+using CheckYourEligibility.Admin.Boundary.Shared;
 using CheckYourEligibility.Admin.Domain.Enums;
 using CheckYourEligibility.Admin.Gateways.Interfaces;
 using CheckYourEligibility.Admin.Models;
@@ -18,13 +19,17 @@ public class SubmitApplicationUseCase : ISubmitApplicationUseCase
 {
     private readonly ILogger<SubmitApplicationUseCase> _logger;
     private readonly IParentGateway _parentGateway;
+    private readonly IBlobStorageGateway _blobStorageGateway;
+    private const string EvidenceContainerName = "fsm-evidence";
 
     public SubmitApplicationUseCase(
         ILogger<SubmitApplicationUseCase> logger,
-        IParentGateway parentGateway)
+        IParentGateway parentGateway,
+        IBlobStorageGateway blobStorageGateway)
     {
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         _parentGateway = parentGateway ?? throw new ArgumentNullException(nameof(parentGateway));
+        _blobStorageGateway = blobStorageGateway ?? throw new ArgumentNullException(nameof(blobStorageGateway));
     }
 
     public async Task<List<ApplicationSaveItemResponse>> Execute(
@@ -33,6 +38,7 @@ public class SubmitApplicationUseCase : ISubmitApplicationUseCase
         string establishment)
     {
         var responses = new List<ApplicationSaveItemResponse>();
+        var evidenceList = await ProcessEvidenceFilesAsync(request);
 
         foreach (var child in request.Children.ChildList)
         {
@@ -54,7 +60,8 @@ public class SubmitApplicationUseCase : ISubmitApplicationUseCase
                         int.Parse(child.Month),
                         int.Parse(child.Day)).ToString("yyyy-MM-dd"),
                     Establishment = int.Parse(establishment),
-                    UserId = userId
+                    UserId = userId,
+                    Evidence = evidenceList
                 }
             };
             var response = await _parentGateway.PostApplication_Fsm(application);
@@ -62,5 +69,35 @@ public class SubmitApplicationUseCase : ISubmitApplicationUseCase
         }
 
         return responses;
+    }
+
+    private async Task<List<ApplicationEvidence>> ProcessEvidenceFilesAsync(FsmApplication request)
+    {
+        var evidenceList = new List<ApplicationEvidence>();
+        if (request.EvidenceFiles != null && request.EvidenceFiles.Count > 0)
+        {
+            foreach (var file in request.EvidenceFiles)
+            {
+                try
+                {
+                    
+                    string blobUrl = await _blobStorageGateway.UploadFileAsync(file, EvidenceContainerName);
+                    
+                    evidenceList.Add(new ApplicationEvidence
+                    {
+                        FileName = file.FileName,
+                        FileType = file.ContentType,
+                        StorageAccountReference = blobUrl
+                    });
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Failed to upload evidence file {FileName}", file.FileName);
+                    // TODO: Handle the error as needed
+                }
+            }
+        }
+
+        return evidenceList;
     }
 }
