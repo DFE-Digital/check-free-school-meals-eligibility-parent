90e9981c745155f976b1708a5b74d2a821867d18
Anoop T (DfE) <anoop.tatti@gmail.com>
2025-05-06 15:21:49 +0100

    feat: added Notification Gateway tests

---
+++ CheckYourEligibility.Admin.Tests/Gateways/DerivedNotificationGateway.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CheckYourEligibility.Admin.Gateways.Tests;
+
+internal class DerivedNotificationGateway : NotificationGateway
+{
+    public DerivedNotificationGateway(ILoggerFactory logger, HttpClient httpClient, IConfiguration configuration)
+        : base(logger, httpClient, configuration)
+    {
+        apiErrorCount = 0;
+    }
+
+    public int apiErrorCount { get; private set; }
+
+    protected override Task LogApiErrorInternal(HttpResponseMessage task, string method, string uri, string data)
+    {
+        apiErrorCount++;
+        return Task.CompletedTask;
+    }
+
+    protected override Task LogApiErrorInternal(HttpResponseMessage task, string method, string uri)
+    {
+        apiErrorCount++;
+        return Task.CompletedTask;
+    }
+}
