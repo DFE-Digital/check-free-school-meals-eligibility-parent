1ec850925dee06d2ff2c8bd01e891afe0e59e7c9
Glen Smith <glen.smith@education.gov.uk>
2025-05-19 23:29:10 +0100

    Add unit tests

---
+++ CheckYourEligibility.FrontEnd.Tests/UseCaseTests/ValidateEvidenceFileUseCaseTests.cs
@@ -0,0 +1,79 @@
+﻿using AutoFixture;
+using CheckYourEligibility.FrontEnd.Usecases;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace CheckYourEligibility.FrontEnd.Tests.UseCases;
+
+[TestFixture]
+public class ValidateEvidenceFileUseCaseTests
+{
+    [SetUp]
+    public void SetUp()
+    {
+        _loggerMock = new Mock<ILogger<ValidateEvidenceFileUseCase>>();
+        _sut = new ValidateEvidenceFileUseCase(_loggerMock.Object);
+        _fixture = new Fixture();
+    }
+
+    private Mock<ILogger<ValidateEvidenceFileUseCase>> _loggerMock;
+    private ValidateEvidenceFileUseCase _sut;
+    private Fixture _fixture;
+
+    [Test]
+    public void Execute_WhenFileTypeIsPdfAndSizeIsValid_ShouldReturnValidResult()
+    {
+        // Arrange
+        var fileMock = new Mock<IFormFile>();
+        var fileName = "good-file.pdf";
+        fileMock.Setup(f => f.FileName).Returns(fileName);
+        fileMock.Setup(f => f.Length).Returns(100);
+        fileMock.Setup(f => f.ContentType).Returns("application/pdf");
+
+        // Act
+        var result = _sut.Execute(fileMock.Object);
+
+        // Assert
+        result.IsValid.Should().BeTrue();
+        result.ErrorMessage.Should().BeEmpty();
+    }
+
+    [Test]
+    public void Execute_WhenFileTypeIsTxtAndSizeIsValid_ShouldReturnInvalidResult()
+    {
+        // Arrange
+        var fileMock = new Mock<IFormFile>();
+        var fileName = "bad-file.txt";
+        fileMock.Setup(f => f.FileName).Returns(fileName);
+        fileMock.Setup(f => f.Length).Returns(100);
+        fileMock.Setup(f => f.ContentType).Returns("plain/text");
+
+        // Act
+        var result = _sut.Execute(fileMock.Object);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.ErrorMessage.Should().Contain("The selected file must be a");
+    }
+
+    [Test]
+    public void Execute_WhenFileTypeIsPdfAndSizeIsInvalid_ShouldReturnInvalidResult()
+    {
+        // Arrange
+        var fileMock = new Mock<IFormFile>();
+        var fileName = "bad-file.pdf";
+        fileMock.Setup(f => f.FileName).Returns(fileName);
+        fileMock.Setup(f => f.Length).Returns(100000000);
+        fileMock.Setup(f => f.ContentType).Returns("application/pdf");
+
+        // Act
+        var result = _sut.Execute(fileMock.Object);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.ErrorMessage.Should().Contain("The selected file must be smaller than");
+    }
+
+}
