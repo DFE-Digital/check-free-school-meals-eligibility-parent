6b5d09ba224c2ff89c9642af43f251ab3ce27359
benfozard-dev <ben.fozard@madetech.com>
2025-09-03 14:59:26 +0100

    Cypress test just for MAT dashboard

--- tests/cypress/e2e/Admin/AdminMatHappyPath.spec.ts
+++ tests/cypress/e2e/Admin/AdminMatHappyPath.spec.ts
@@ -11,50 +11,15 @@ it('Will allow an MAT user to create an application is eligible and submit an ap
         cy.checkSession('MAT');
         cy.visit(Cypress.config().baseUrl ?? "");
         cy.get('h1').should('include.text', 'THOMAS TELFORD MULTI ACADEMY TRUST');
-        //Add parent details
-        cy.contains('Run a check for one parent or guardian').click();
-        cy.url().should('include', '/Check/Enter_Details');
-        cy.get('#FirstName').type(parentFirstName);
-        cy.get('#LastName').type(parentLastName);
-        cy.get('#EmailAddress').type(parentEmailAddress);
-        cy.get('#Day').type('01');
-        cy.get('#Month').type('01');
-        cy.get('#Year').type('1990');
-        cy.get('#NinAsrSelection').click();
-        cy.get('#NationalInsuranceNumber').type("nn123456c");
-        cy.contains('button', 'Perform check').click();
+        
+        //Assert that MAT dashboard has same actions as LA dashboard
+        cy.contains('Run a check for one parent or guardian');
+        cy.contains('Run a batch check');
+        cy.contains('Pending applications');
+        cy.contains('Search all records');
+        cy.contains('Guidance for reviewing evidence');
 
-        //Loader page
-        cy.url().should('include', 'Check/Loader');
+        //Future work should extend to perform check
 
-        //Eligible outcome page
-        cy.get('.govuk-body', { timeout: 80000 }).should('include.text', 'This information should be passed on to their school.');
-        cy.get('a.govuk-link').contains('Continue to add child details').click();
-        //Enter child details
-        cy.url().should('include', '/Enter_Child_Details');
-        cy.get('[id="ChildList[0].FirstName"]').type(childFirstName);
-        cy.get('[id="ChildList[0].LastName"]').type(childLastName);
-        cy.get('[id="ChildList[0].Day"]').type('01');
-        cy.get('[id="ChildList[0].Month"]').type('01');
-        cy.get('[id="ChildList[0].Year"]').type('2007');
-        cy.get('[id="ChildList[0].School"]').type('The Telford Park School');
-
-        cy.get('#schoolList0')
-            .should('be.visible')
-            .contains('The Telford Park School, 150716, TF3 1FA, Telford and Wrekin')
-            .click({ force: true });
-             cy.contains('button', 'Save and continue').click();
-
-        //Check answers page
-        cy.get('h1').should('include.text', 'Check your answers before submitting');
-        cy.CheckValuesInSummaryCard('Parent or guardian details', 'Name', `${parentFirstName} ${parentLastName}`);
-        cy.CheckValuesInSummaryCard('Parent or guardian details', 'Date of birth', '1 January 1990');
-        cy.CheckValuesInSummaryCard('Parent or guardian details', 'National Insurance number', "NN123456C");
-        cy.CheckValuesInSummaryCard('Parent or guardian details', 'Email address', parentEmailAddress);
-        cy.CheckValuesInSummaryCard('Child 1 details', "Name", childFirstName + " " + childLastName);
-        cy.contains('button', 'Add details').click();
-
-        //Applications Registered confirmation page
-        cy.url().should('include', '/Check/ApplicationsRegistered');
     });
 })
