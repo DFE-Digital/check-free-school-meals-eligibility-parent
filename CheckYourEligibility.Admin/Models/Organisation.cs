ae866b637f44002d4cf5dbb34cfff32165b92978
Jenna Barton <jenna.barton@education.gov.uk>
2025-11-04 15:03:17 +0000

    Create enum and encapsulation for OrganisationType to describe LA, MAT and School.

---
+++ CheckYourEligibility.Admin/Models/Organisation.cs
@@ -0,0 +1,13 @@
+﻿using CheckYourEligibility.Admin.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace CheckYourEligibility.Admin.Models;
+
+public class Organisation
+{
+    public OrganisationType Type { get; set; }
+
+    public bool IsLA => Type == OrganisationType.LA;
+    public bool IsMAT => Type == OrganisationType.MAT;
+    public bool IsSchool => Type == OrganisationType.School;
+}
