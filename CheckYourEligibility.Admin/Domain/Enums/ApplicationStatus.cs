9d2ca8f6a81f1b2fdcf8a6043d5256855f8608e6
Jenna Barton <jenna.barton@education.gov.uk>
2025-12-15 14:35:10 +0000

    Change casing on all statuses

--- CheckYourEligibility.Admin/Domain/Enums/ApplicationStatus.cs
+++ CheckYourEligibility.Admin/Domain/Enums/ApplicationStatus.cs
@@ -7,10 +7,10 @@ namespace CheckYourEligibility.Admin.Domain.Enums;
 public enum ApplicationStatus
 {
     [Description("Entitled")] Entitled,
-    [Description("Receiving Entitlement")] Receiving,
-    [Description("Evidence Needed")] EvidenceNeeded,
-    [Description("Sent for Review")] SentForReview,
-    [Description("Reviewed Entitled")] ReviewedEntitled,
-    [Description("Reviewed Not Entitled")] ReviewedNotEntitled,
+    [Description("Receiving entitlement")] Receiving,
+    [Description("Evidence needed")] EvidenceNeeded,
+    [Description("Sent for review")] SentForReview,
+    [Description("Reviewed entitled")] ReviewedEntitled,
+    [Description("Reviewed not entitled")] ReviewedNotEntitled,
     [Description("Archived")] Archived
 }
