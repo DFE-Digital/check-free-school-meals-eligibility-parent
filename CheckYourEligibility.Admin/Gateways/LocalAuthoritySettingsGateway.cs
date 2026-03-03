3306959291c9b8bf07ca1c709d1718c9a561d5b9
GEORGANTAS, Yiannos <Ioannis.GEORGANTAS@EDUCATION.GOV.UK>
2026-03-03 11:06:24 +0000

    Add gateway

---
+++ CheckYourEligibility.Admin/Gateways/LocalAuthoritySettingsGateway.cs
@@ -0,0 +1,27 @@
+﻿using CheckYourEligibility.Admin.Boundary.Responses;
+using CheckYourEligibility.Admin.Gateways.Interfaces;
+
+namespace CheckYourEligibility.Admin.Gateways;
+
+public sealed class LocalAuthoritySettingsGateway : BaseGateway, ILocalAuthoritySettingsGateway
+{
+    private readonly HttpClient _httpClient;
+
+    public LocalAuthoritySettingsGateway(
+        ILoggerFactory logger,
+        HttpClient httpClient,
+        IConfiguration configuration,
+        IHttpContextAccessor httpContextAccessor)
+        : base("EcsService", logger, httpClient, configuration, httpContextAccessor)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<bool> GetSchoolCanReviewEvidenceAsync(int laCode)
+    {
+        var url = $"{_httpClient.BaseAddress}local-authorities/{laCode}/settings";
+
+        var response = await ApiDataGetAsynch(url, new LocalAuthoritySettingsResponse());
+        return response?.SchoolCanReviewEvidence ?? false;
+    }
+}
