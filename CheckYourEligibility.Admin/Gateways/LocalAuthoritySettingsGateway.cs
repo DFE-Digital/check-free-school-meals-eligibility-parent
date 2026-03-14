2054fed998ee91b32140f373d54f257ab2370837
YiannosGeorgantas <ioannis.georgantas@education.gov.uk>
2026-03-14 22:21:07 +0000

    implementation of new gateway

---
+++ CheckYourEligibility.Admin/Gateways/LocalAuthoritySettingsGateway.cs
@@ -0,0 +1,26 @@
+﻿using CheckYourEligibility.Admin.Boundary.Responses;
+using CheckYourEligibility.Admin.Gateways.Interfaces;
+
+namespace CheckYourEligibility.Admin.Gateways;
+
+public sealed class LocalAuthoritySettingsGateway : BaseGateway, ILocalAuthoritySettingsGateway
+{
+
+
+    public LocalAuthoritySettingsGateway(
+        ILoggerFactory logger,
+        HttpClient httpClient,
+        IConfiguration configuration,
+        IHttpContextAccessor httpContextAccessor)
+        : base("EcsService", logger, httpClient, configuration, httpContextAccessor)
+    {
+    }
+
+    public async Task<bool> GetSchoolCanReviewEvidenceAsync(int laCode)
+    {
+        var url = $"local-authorities/{laCode}/settings";
+
+        var response = await ApiDataGetAsynch(url, new LocalAuthoritySettingsResponse());
+        return response?.SchoolCanReviewEvidence ?? false;
+    }
+}
