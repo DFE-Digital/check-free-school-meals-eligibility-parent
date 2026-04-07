647269d0621f5e7630fd3bcd3db8475f9a381204
YiannosGeorgantas <ioannis.georgantas@education.gov.uk>
2026-04-07 11:56:27 +0100

    removed "finalise " tile from tests' expectations. Changed to more appropriate test titles

--- tests/cypress/e2e/Admin/AdminReviewEvidenceVisibility.spec.ts
+++ tests/cypress/e2e/Admin/AdminReviewEvidenceVisibility.spec.ts
@@ -6,21 +6,19 @@ describe('Log in as schools whose LA has set that they can or cannot review evid
         cy.get('.govuk-caption-l').should('include.text', 'The Telford Park School');
         cy.get('h1').should('include.text', 'Manage eligibility for free school meals');
         cy.contains('a', 'Pending applications').should('be.visible');
-        cy.contains('a', 'Finalise applications').should('be.visible');
         cy.contains('a', 'Guidance for reviewing evidence')
             .should('be.visible')
             .and('have.attr', 'href')
             .and('include', '/Home/Guidance');
     });
 
-    it('does not show review tiles for schools whose LA has the flag disabled', () => {
+    it('does not show review tiles for non-MAT schools whose LA flag is disabled', () => {
         cy.checkSession('schoolCanReviewEvidenceDisabled');
         cy.visit((Cypress.config().baseUrl ?? "") + "/home");
         cy.wait(1);
         cy.get('.govuk-caption-l').should('include.text', 'The Astley Cooper School');
         cy.get('h1').should('include.text', 'Manage eligibility for free school meals');
         cy.contains('a', 'Pending applications').should('not.exist');
-        cy.contains('a', 'Finalise applications').should('not.exist');
         cy.contains('a', 'Guidance for reviewing evidence').should('not.exist');
     });
 });
