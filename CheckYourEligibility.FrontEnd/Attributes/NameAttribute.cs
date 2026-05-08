d46366ecf062409c1224a66db49e475e0817b9f7
YiannosGeorgantas <ioannis.georgantas@education.gov.uk>
2026-05-08 19:07:40 +0100

    NameAttribute fix for apostrophes

--- CheckYourEligibility.FrontEnd/Attributes/NameAttribute.cs
+++ CheckYourEligibility.FrontEnd/Attributes/NameAttribute.cs
@@ -5,7 +5,7 @@ namespace CheckYourEligibility.FrontEnd.Attributes;
 
 public class NameAttribute : ValidationAttribute
 {
-    public static readonly string UnicodeOnlyPattern = @"^[a-zA-Z ,.'-]+$";
+    public static readonly string UnicodeOnlyPattern = @"^[a-zA-Z ,.'\u2018\u2019-]+$";
 
     private static readonly Regex regex = new(UnicodeOnlyPattern);
 
