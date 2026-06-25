921fc3efa87d425524ca42127e5cc75611dcb856
Jenna Barton <jenna.barton@education.gov.uk>
2026-06-25 13:47:05 +0100

    Add Extentions and tests for time display and update Application_Sent to use it. NOTE-Extention modified to include 'at' between date and time for Parent Portal only.

---
+++ CheckYourEligibility.FrontEnd.Tests/Models/ExtensionsTests.cs
@@ -0,0 +1,121 @@
+﻿using CheckYourEligibility.FrontEnd.Models;
+using FluentAssertions;
+
+
+namespace CheckYourEligibility.Admin.Tests.Models
+{ 
+    [TestFixture]
+    public class DateTimeExtensionsTests
+    {
+
+        [Test]
+        public void GetLocalTime_With_UTC_Date_In_Winter_Should_Return_GMT_Time()
+        {
+            // Arrange
+            var utcDate = new DateTime(2026, 1, 15, 10, 30, 0, DateTimeKind.Utc);
+
+            // Act
+            var result = DateTimeExtensions.GetLocalTime(utcDate);
+
+            // Assert
+            result.Hour.Should().Be(10);
+            result.Minute.Should().Be(30);
+        }
+
+        [Test]
+        public void GetLocalTime_With_UTC_Date_In_Summer_Should_Return_BST_Time()
+        {
+            // Arrange
+            var utcDate = new DateTime(2026, 6, 15, 10, 30, 0, DateTimeKind.Utc);
+
+            // Act
+            var result = DateTimeExtensions.GetLocalTime(utcDate);
+
+            // Assert
+            result.Hour.Should().Be(11);
+            result.Minute.Should().Be(30);
+        }
+
+        [Test]
+        public void GetUTCTime_With_Utc_Date_Should_Return_Same_Date()
+        {
+            // Arrange
+            var utcDate = new DateTime(2026, 2, 10, 14, 0, 0, DateTimeKind.Utc);
+
+            // Act
+            var result = DateTimeExtensions.GetUTCTime(utcDate);
+
+            // Assert
+            result.Should().Be(utcDate);
+            result.Kind.Should().Be(DateTimeKind.Utc);
+        }
+
+        [Test]
+        public void GetUTCTime_With_Local_BST_Date_Should_Convert_To_Utc()
+        {
+            // Arrange
+            // 15 June 2026 11:00 BST = 10:00 UTC
+            var localDate = new DateTime(2026, 6, 15, 11, 0, 0, DateTimeKind.Unspecified);
+
+            // Act
+            var result = DateTimeExtensions.GetUTCTime(localDate);
+
+            // Assert
+            result.Hour.Should().Be(10);
+            result.Kind.Should().Be(DateTimeKind.Utc);
+        }
+
+        [Test]
+        public void ToLocalString12HourFormatReadable_Should_Format_GMT_Date_Correctly()
+        {
+            // Arrange
+            var utcDate = new DateTime(2026, 2, 5, 14, 5, 0, DateTimeKind.Utc);
+
+            // Act
+            var result = utcDate.ToLocalString12HourFormatReadable();
+
+            // Assert
+            result.Should().Be("05 Feb 2026 02:05pm");
+        }
+
+        [Test]
+        public void ToLocalString12HourFormatReadable_Should_Format_BST_Date_Correctly()
+        {
+            // Arrange
+            var utcDate = new DateTime(2026, 7, 5, 14, 5, 0, DateTimeKind.Utc);
+
+            // Act
+            var result = utcDate.ToLocalString12HourFormatReadable();
+
+            // Assert
+            result.Should().Be("05 Jul 2026 03:05pm");
+        }
+
+        [Test]
+        public void GetDateTimeOffsetFromString_In_Winter_Should_Have_Zero_Offset()
+        {
+            // Arrange
+            var input = "2026-01-20 09:00";
+
+            // Act
+            var result = DateTimeExtensions.GetDateTimeOffsetFromString(input);
+
+            // Assert
+            result.Offset.Should().Be(TimeSpan.Zero);
+        }
+
+        [Test]
+        public void GetDateTimeOffsetFromString_In_Summer_Should_Have_One_Hour_Offset()
+        {
+            // Arrange
+            var input = "2026-06-20 09:00";
+
+            // Act
+            var result = DateTimeExtensions.GetDateTimeOffsetFromString(input);
+
+            // Assert
+            result.Offset.Should().Be(TimeSpan.FromHours(1));
+        }
+    }
+
+}
---
+++ CheckYourEligibility.FrontEnd/Models/Extensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace CheckYourEligibility.FrontEnd.Models;
+
+public static class DateTimeExtensions
+{
+    public static TimeZoneInfo TimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timezone);
+
+    private const string timezone = "GMT Standard Time";
+
+    public static DateTimeOffset GetDateTimeOffsetFromString(string datetime)
+    {
+        DateTime raw = DateTime.ParseExact(datetime, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None);
+
+        DateTimeOffset offset = new DateTimeOffset(raw, TimeZoneInfo.BaseUtcOffset);
+        var rules = TimeZoneInfo.GetAdjustmentRules();
+        if (TimeZoneInfo.IsDaylightSavingTime(raw))
+        {
+            offset = new DateTimeOffset(raw.AddHours(-1), rules.First().DaylightDelta);
+        }
+        return offset;
+    }
+    public static DateTime GetLocalTime(DateTime time)
+    {
+        return TimeZoneInfo.ConvertTimeFromUtc(time, TimeZoneInfo);
+    }
+
+    public static DateTime GetUTCTime(DateTime time)
+    {
+        if (time.Kind != DateTimeKind.Utc)
+        {
+            time = TimeZoneInfo.ConvertTimeToUtc(time);
+        }
+        return time;
+    }
+    public static string ToLocalString12HourFormatReadableWithAt(this DateTime datetime)
+    {
+        return GetLocalTime(datetime).ToString("dd MMM yyyy 'at' h:mmtt").Replace("AM", "am").Replace("PM", "pm");
+    }
+}
