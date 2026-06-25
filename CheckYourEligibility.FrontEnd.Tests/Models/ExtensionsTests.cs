6953f88843fb5ce9b7fb18f973f0b44cf537e8ea
Jenna Barton <jenna.barton@education.gov.uk>
2026-06-25 15:43:03 +0100

    Removed too many zero's previously and forgot the 'at'.

--- CheckYourEligibility.FrontEnd.Tests/Models/ExtensionsTests.cs
+++ CheckYourEligibility.FrontEnd.Tests/Models/ExtensionsTests.cs
@@ -75,7 +75,7 @@ namespace CheckYourEligibility.Admin.Tests.Models
             var result = utcDate.ToLocalString12HourFormatReadableWithAt();
 
             // Assert
-            result.Should().Be("05 Feb 2026 2:05pm");
+            result.Should().Be("05 Feb 2026 at 2:05pm");
         }
 
         [Test]
@@ -88,14 +88,14 @@ namespace CheckYourEligibility.Admin.Tests.Models
             var result = utcDate.ToLocalString12HourFormatReadableWithAt();
 
             // Assert
-            result.Should().Be("05 Jul 2026 3:05pm");
+            result.Should().Be("05 Jul 2026 at 3:05pm");
         }
 
         [Test]
         public void GetDateTimeOffsetFromString_In_Winter_Should_Have_Zero_Offset()
         {
             // Arrange
-            var input = "2026-01-20 9:00";
+            var input = "2026-01-20 09:00";
 
             // Act
             var result = DateTimeExtensions.GetDateTimeOffsetFromString(input);
@@ -108,7 +108,7 @@ namespace CheckYourEligibility.Admin.Tests.Models
         public void GetDateTimeOffsetFromString_In_Summer_Should_Have_One_Hour_Offset()
         {
             // Arrange
-            var input = "2026-06-20 9:00";
+            var input = "2026-06-20 09:00";
 
             // Act
             var result = DateTimeExtensions.GetDateTimeOffsetFromString(input);
